cfe40cb24ce4acde7336c13b073a26ba2295c0d3
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2022-12-06 23:17:09 -0600

    refactor: loop and project settings

--- renderer3d/main.c
+++ renderer3d/main.c
@@ -1,32 +1,11 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <SDL.h>
+#include "display.h"
 
 bool is_running = false;
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-
-bool initialize_window(void)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		fprintf(stderr, "Error initializing SDL.\n");
-		return false;
-	}
-	window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
-	if (!window) {
-		fprintf(stderr, "Error creating SDL Window\n");
-		return false;
-	}
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	if (!renderer) {
-		fprintf(stderr, "Error creating SDL Renderer\n");
-		return false;
-	}
-	return true;
-}
 
 void setup(void) {
-	// setup function
+	// Alocate the required memory in bytes for the color_buffer used to create the renderer texture.
+	color_buffer = (uint32_t*) malloc(sizeof (uint32_t) * window_width * window_height);
+	color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, window_width, window_height);
 }
 
 void process_input(void) {
@@ -50,11 +29,17 @@ void update(void) {
 }
 
 void render(void) {
-	// render function
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	draw_grid();
+	draw_pixel(205, 205, 0xFFAF2A6E);
+	draw_rect(100, 100, 100, 100, 0xFFD84242);
+	render_color_buffer();
+	clear_color_buffer(0xFFEAFF7B);
+	SDL_RenderPresent(renderer);
 }
 
-int main(int argc, char* args)
-{
+int main(int argc, char* args) {
 	is_running = initialize_window();
 	setup();
 	while (is_running) {
@@ -62,5 +47,6 @@ int main(int argc, char* args)
 		update();
 		render();
 	}
+	destroy_window();
 	return 0;
 }
